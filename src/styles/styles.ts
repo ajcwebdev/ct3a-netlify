29c4ee1c6fa9941053ab1aceec93bbddad1dbca7
Anthony Campolo <admin@ajcwebdev.com>
2022-11-06 11:49:43 -0600

    n3tlify

---
+++ src/styles/styles.ts
@@ -0,0 +1,14 @@
+const appContainer = "container mx-auto flex min-h-screen flex-col items-center justify-center p-4"
+const title = "text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]"
+const purple = "text-purple-300"
+const body = "text-2xl text-gray-700"
+const grid = "mt-3 grid gap-3 pt-3 text-center md:grid-cols-2 lg:w-2/3"
+const queryResponse = "flex w-full items-center justify-center pt-6 text-2xl text-blue-500"
+const cardSection = "flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105"
+const cardTitle = "text-lg text-gray-700"
+const cardDescription = "text-sm text-gray-600"
+const link = "m-auto mt-3 w-fit text-sm text-violet-500 underline decoration-dotted underline-offset-2"
+
+export {
+  appContainer, title, purple, body, grid, queryResponse, cardSection, cardTitle, cardDescription, link
+}
